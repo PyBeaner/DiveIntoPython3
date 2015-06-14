2b5d289c4c41f5513741f7736b0bdfe47e3b662e
PyBeaner <758046738@qq.com>
2015-06-14 16:36:02 +0800

    l

--- 3.Comprehensions/list_comprehensions.py
+++ 3.Comprehensions/list_comprehensions.py
@@ -1,1 +1,12 @@
 __author__ = 'PyBeaner'
+
+a_list = [1,9,8,4]
+print([elem*2 for elem in a_list])
+print(a_list)
+a_list = [elem*2 for elem in a_list]
+print(a_list)
+
+import os,glob
+print(glob.glob("*.py"))
+print([os.path.realpath(p) for p in glob.glob("*.py")])
+print([os.path.realpath(p) for p in glob.glob("*.py") if os.stat(p).st_size>500])
