d30c88a9073d747019aacdedf8f7650abcf24e4b
PyBeaner <758046738@qq.com>
2015-06-14 16:10:50 +0800

    .

--- 2.Native Datatypes/none.py
+++ 2.Native Datatypes/none.py
@@ -1,1 +1,16 @@
 __author__ = 'PyBeaner'
+
+# None is not the same as False. None is not 0.
+# None is not an empty string.
+# Comparing None to anything other than None will always return False.
+# You can assign None to any variable, but you can not create other NoneType objects
+# All variables whose value is None are equal to each other
+
+print(type(None)) # NoneType
+print(None==False) # False
+print(None==0)
+print(None==None) # True
+x = None
+print(x==None) # True
+y = None
+print(x==y) # True
