40a918faa9d5eb5237008a02b202574772afeff8
PyBeaner <758046738@qq.com>
2015-06-14 20:30:42 +0800

    first nth fibs

--- 6.Closures & Generators/fibonacci.py
+++ 6.Closures & Generators/fibonacci.py
@@ -2,37 +2,39 @@ __author__ = 'PyBeaner'
 
 # very slow
 def recursive_fib(n):
-    if n<=1:
-        return n
+    if n<=2:
+        return n-1
     return recursive_fib(n-1)+recursive_fib(n-2)
 
-def fib(max):
+def fib(n,max=0):
     a,b = 0,1
-    while a<max:
+    i = 0
+    while i<n:
+        if max and a>max:
+            break
         yield a
         a,b = b,a+b
+        i+=1
 
 if __name__=="__main__":
     import time
     max = 1000000
     start = time.clock()
-    for n in fib(max):
-        print(n,end=" ")
+    count = 20
+    fibs = list(fib(count))
+    print(fibs[-1])
+    # for n in fib(20):
+    #     print(n,end=" ")
     end = time.clock()
     print()
-    #  0.000092.2 seconds
-    print("Fib(yield) spent %f.2 seconds" %(end-start))
+    #  0.000033 seconds
+    print("Fib(yield) spent %f seconds" %(end-start))
 
     print()
-    n = 0
     start = time.clock()
-    while True:
-        f = recursive_fib(n)
-        if f>max:
-            break
-        print(f,end=" ")
-        n+=1
+    r = recursive_fib(count)
+    print(r)
     end = time.clock()
     print()
-    #  2.408104.2 seconds
-    print("Fib(recursive) spent %f.2 seconds" %(end-start))
+    #  0.003518 seconds
+    print("Fib(recursive) spent %f seconds" %(end-start))
