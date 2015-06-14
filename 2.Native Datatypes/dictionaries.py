4c1a873d09292c5876a6d7f4edfb45e818ee2b22
PyBeaner <758046738@qq.com>
2015-06-14 16:06:10 +0800

    dict

---
+++ 2.Native Datatypes/dictionaries.py
@@ -0,0 +1,9 @@
+__author__ = 'PyBeaner'
+
+a_dict = {'server': 'db.diveintopython3.org', 'database': 'mysql'}
+print(a_dict)
+print(type(a_dict))
+
+# Modification
+a_dict['database'] = "sqlite3"
+print(a_dict)
