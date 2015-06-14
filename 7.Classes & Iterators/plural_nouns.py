ade89a413ff923a3007ea5db6b4c867849f15338
PyBeaner <758046738@qq.com>
2015-06-14 18:36:15 +0800

    replace functions with comprehensions

--- 7.Classes & Iterators/plural_nouns.py
+++ 7.Classes & Iterators/plural_nouns.py
@@ -7,36 +7,24 @@ def plural(noun):
         if match_rule(noun):
             return apply_rule(noun)
 
-def match_sxz(noun):
-    return re.search("[sxz]$",noun)
 
-def apply_sxz(noun):
-    return re.sub("$","es",noun)
+def build_match_and_apply_function(pattern,search,replace):
+    def match_rule(word):
+        return re.search(pattern,word)
 
-def match_h(noun):
-    return re.search('[^aeioudgkprt]h$', noun)
+    def apply_rule(word):
+        return re.sub(search,replace,word)
 
-def apply_h(noun):
-    return re.sub('$',"es",noun)
+    return match_rule,apply_rule
 
-def match_y(noun):
-    return re.search("[^aeiou]y$",noun)
-
-def apply_y(noun):
-    return re.sub("y$","ies",noun)
-
-def match_default(noun):
-    return True
-
-def apply_default(noun):
-    return noun+"s"
-
-rules = (
-    (match_sxz,apply_sxz),
-    (match_h,apply_h),
-    (match_y,apply_y),
-    (match_default,apply_default)
+patterns = (
+    ("[sxz]$","$","es"),
+    ("[^aeioudgkprt]h$","$","es"),
+    ("(qu[^aeiou])y$","y$","ies"),
+    ("$","$","s"),
 )
+
+rules = [build_match_and_apply_function(pattern,search,replace) for pattern,search,replace in patterns]
 
 if __name__ == "__main__":
     print(plural("fox"))
