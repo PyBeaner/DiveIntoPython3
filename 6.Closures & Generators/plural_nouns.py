3b9d9040bdad1f0d19468be67a6e3d1776fe5c19
PyBeaner <758046738@qq.com>
2015-06-14 18:50:12 +0800

    yield rules

--- 6.Closures & Generators/plural_nouns.py
+++ 6.Closures & Generators/plural_nouns.py
@@ -1,11 +1,14 @@
+# coding=utf-8
 __author__ = 'PyBeaner'
 
 import re
 
 def plural(noun):
-    for match_rule,apply_rule in rules:
+    for match_rule,apply_rule in rules("plural-rule.txt"):
         if match_rule(noun):
             return apply_rule(noun)
+
+    raise ValueError("no matching rule for {0}".format(noun))
 
 
 def build_match_and_apply_function(pattern,search,replace):
@@ -17,12 +20,12 @@ def build_match_and_apply_function(pattern,search,replace):
 
     return match_rule,apply_rule
 
-rules = []
-with open("plural-rule.txt") as file:
-    for line in file:
-        pattern,search,replace = line.split(None,3) # split by any whitespaces, and take 3 sections
-        rules.append(build_match_and_apply_function(
-            pattern,search,replace))
+def rules(rules_filename):
+    with open(rules_filename) as file:
+        for line in file:
+            pattern,search,replace = line.split(None,3) # split by any whitespaces, and take 3 sections
+            yield build_match_and_apply_function(
+                pattern,search,replace)
 
 
 if __name__ == "__main__":
