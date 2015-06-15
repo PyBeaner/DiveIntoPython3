86a29751a5b68ce05c8e0bd951dcc6be669a11a5
XiaomingSu <xiaoming.su@patpat.com>
2015-06-15 12:32:41 +0800

    example

---
+++ 11.Files/example.py
@@ -0,0 +1,14 @@
+import locale
+
+__author__ = 'PyBeaner'
+
+# a_file = open("example.py",encoding="utf-8")
+a_file = open("example.py")
+# example.py
+print(a_file.name)
+# utf-8
+# default:cp936(depends on your Operation System and the language)
+print(a_file.encoding)
+print(locale.getpreferredencoding()) # cp936
+# r
+print(a_file.mode)
