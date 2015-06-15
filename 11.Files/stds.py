8140b36e8cc457d0f96bdcc2d917de7945640732
XiaomingSu <xiaoming.su@patpat.com>
2015-06-15 13:16:54 +0800

    stderr,stdout

---
+++ 11.Files/stds.py
@@ -0,0 +1,14 @@
+__author__ = 'PyBeaner'
+
+for i in range(3):
+    print("PapayaWhip")
+
+import sys
+for i in range(3):
+    l = sys.stdout.write("is the") # return the count writen
+
+for i in range(3):
+    l = sys.stderr.write("new black");
+
+# io.UnsupportedOperation: not readable
+# sys.stdout.read()
