20fe7d5db7eb48149afcc2646990a41b4c8835c1
XiaomingSu <xiaoming.su@patpat.com>
2015-06-15 14:59:59 +0800

    pickle version

--- 13.Serializing Python Objects/pickle_protocol.py
+++ 13.Serializing Python Objects/pickle_protocol.py
@@ -3,6 +3,16 @@ __author__ = 'PyBeaner'
 
 import pickletools
 import pickle
+
+
+def protocol_version(file_object):
+    maxproto = -1
+    for opcode,arg,pos in pickletools.genops(file_object):
+        maxproto = max(maxproto,opcode.proto)
+    return maxproto
+
 with open("entry.pickle","rb") as f:
-    r = pickletools.dis(f)
-    print(r)
+    # r = pickletools.dis(f)
+    # print(r)  # highest protocol among opcodes = 3
+    v = protocol_version(f)
+    print(v)  # 3
