389a057ffe2f8e7b2c92c874cef59510c14fa387
XiaomingSu <xiaoming.su@patpat.com>
2015-06-15 14:32:06 +0800

    .

--- 13.Serializing Python Objects/shell1.py
+++ 13.Serializing Python Objects/shell1.py
@@ -1,1 +1,19 @@
 __author__ = 'PyBeaner'
+
+shell = 1
+
+entry = {}
+entry['title'] = 'Dive into history, 2009 edition'
+entry['article_link'] = 'http://diveintomark.org/archives/2009/03/27/dive-into-history-2009-edition'
+entry['comments_link'] = None
+entry['internal_id'] = b'\xDE\xD5\xB4\xF8'
+entry['tags'] = ('diveintopython', 'docbook', 'html')
+entry['published'] = True
+import time
+entry['published_date'] = time.strptime('Fri Mar 27 22:20:42 2009')
+print(entry['published_date'])
+
+import pickle
+
+with open("entry.pickle","wb") as f:
+    pickle.dump(entry,f)
--- 13.Serializing Python Objects/shell2.py
+++ 13.Serializing Python Objects/shell2.py
@@ -1,1 +1,10 @@
 __author__ = 'PyBeaner'
+
+shell = 2
+
+import pickle
+
+with open("entry.pickle","rb") as f:
+    entry = pickle.load(f)
+
+print(entry)
